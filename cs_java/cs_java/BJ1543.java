c6a00d82091bc8b0d2e49828e481740bd0cffeff
JungHoiMin <wony1102@naver.com>
2022-10-19 19:25:56 +0900

    221010 code upload

--- cs_java/cs_java/BJ1543.java
+++ cs_java/cs_java/BJ1543.java
@@ -1,12 +1,37 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
+// 문서 검색 https://www.acmicpc.net/problem/1543
 public class BJ1543 {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    // 문서의 시작 인덱스부터 단어의 길이까지 단어가 포함이 되는지 확인하는 함수
+    static boolean check(String origin, String find, int s){
+        boolean flag = true;
 
+        // 한 글자씩 확인
+        for (int i = 0; i<find.length(); i++) {
+            // 글자가 같지 않다면 flag 체크 후 중지
+            if (origin.charAt(i+s) != find.charAt(i)){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 문서
+        String origin = br.readLine();
+        // 단어
+        String find = br.readLine();
+
+        int cnt = 0;
+        for (int i=0;i<origin.length()-find.length()+1; i++){
+            if (check(origin,find,i)){
+                cnt++;
+                i += find.length()-1;
+            }
+        }
+
+        System.out.println(cnt);
+        br.close();
     }
 }
--- cs_java/cs_java/BJ2457.java
+++ cs_java/cs_java/BJ2457.java
@@ -1,5 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 공주님의 정원 https://www.acmicpc.net/problem/2457
 public class BJ2457 {
-    public static void main(String[] args) {
-        System.out.println("Bye");
+    static class Pair {
+        private final int x, y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        int first() {
+            return x;
+        }
+
+        int second() {
+            return y;
+        }
+
+        static Pair of(int x, int y) {
+            return new Pair(x, y);
+        }
+    }
+
+    static int N;
+    static List<Pair> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int day1 = Integer.parseInt(st.nextToken()) * 100 + Integer.parseInt(st.nextToken());
+            int day2 = Integer.parseInt(st.nextToken()) * 100 + Integer.parseInt(st.nextToken());
+            list.add(Pair.of(day1, day2));
+        }
+
+        list.sort((o1, o2) -> {
+            if (o1.first() == o2.first()) return o2.second() - o1.second();
+            return o1.first() - o2.first();
+        });
+        int start = 301, end = 0;
+        int current = 0, answer = 0;
+
+        while (start < 1201) {
+            boolean change = false;
+            for (int i = current; i < N; i++) {
+                if (list.get(i).first() <= start && list.get(i).second() > end) {
+                    end = list.get(i).second();
+                    current = i + 1;
+                    change = true;
+                }
+            }
+            if (change) {
+                start = end;
+                answer++;
+            } else {
+                break;
+            }
+        }
+        if (end < 1201) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
+        br.close();
     }
 }
